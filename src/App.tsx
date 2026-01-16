7f57f3a78405c7e63b4c9cdc1434693a4cca613c
Selbi <selbibegiyeva1@gmail.com>
2026-01-16 11:35:43 +0500

    NEW ROUTE COMMIT

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 
 import Login from './pages/auth/Login'
+
 import Home from './pages/operator/Home'
+import Product from './pages/operator/Product'
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
 
         {/* Operator Routes */}
         <Route path="/operator/home" element={<Home />} />
+        <Route path="/operator/product" element={<Product />} />
       </Routes>
     </div>
   )
